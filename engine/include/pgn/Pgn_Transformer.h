db9c691bb1eac99c3539790dbd62b4cce8019280
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-03-28 13:22:03 +0800

    removed unused function.

--- engine/include/pgn/Pgn_Transformer.h
+++ engine/include/pgn/Pgn_Transformer.h
@@ -7,8 +7,6 @@
 extern const char colToPgn[8];
 extern const char rowToPgn[8];
 
-bool isStringAPosition(std::string s);
-
 Position pgnToPosition(std::string s);
 
 std::string positionToPgn(Position pos);
