beb9843d847c744e5f700c0e0a8f17ef56b6088c
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-04-01 22:16:02 +0800

    add `UndoState` and `MoveState`

---
+++ engine/include/move/Make_BitMove.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "board/Board.h"
+#include <cstdint>
+#include <board/Piece.h>
+
+struct UndoState {
+    uint8_t castleRights;
+    int materialScore;
+    int PSTScore;
+    uint64_t zobristKey;
+    Piece capturedPiece;
+};
+
+// intergret move infos
+// placedPiece: The piece that actually placed at Position to
+struct MoveState {
+    Position from, to;
+
+    Piece movePiece, capturedPiece, placedPiece;
+
+    Player player;
+    
+    bool isCastle, isPromotion;
+};
