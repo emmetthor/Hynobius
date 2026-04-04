b192390042f4fbdc88e8dd33dc9cd7ba3b59d10c
emmetthor <80567173+emmetthor@users.noreply.github.com>
2026-04-04 16:20:50 +0800

    add promotion perft

--- tests/Perft.cpp
+++ tests/Perft.cpp
@@ -46,17 +46,19 @@ int main()
     std::vector<std::string> fenList = {
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
         "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-        "7k/8/8/8/8/8/7P/4K2R w K - 0 1"};
-    std::vector<int> validTests = {6, 0, 1};
+        "7k/8/8/8/8/8/7P/4K2R w K - 0 1",
+        "7k/PPPPPP1p/5p1K/5PpP/6P1/8/8/8 w - - 0 1"
+    };
+    std::vector<int> validTests = {6, 0, 1, 1};
     std::vector<std::vector<int>> result = {
-        {1, 20, 400, 8902, 197281, 4865609}, {1, 48, 2039, 97862, 4085603, 193690690}, {1, 10}};
+        {1, 20, 400, 8902, 197281, 4865609}, {1, 48, 2039, 97862, 4085603, 193690690}, {1, 10}, {1, 24}};
     std::vector<std::vector<int>> enPassantDiff = {
-        {0, 0, 0, 0, 0, 258}, {0, 0, 1, 45, 1929, 73365, 3577504}, {0, 0}};
+        {0, 0, 0, 0, 0, 258}, {0, 0, 1, 45, 1929, 73365, 3577504}, {0, 0}, {0, 0}};
     int totalTestCases = 0;
     int failedTestCases = 0;
     int expected = 0;
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 4; i++)
     {
         auto res = testPerft(fenList[i], std::min(doDepth, validTests[i]));
         for (int j = 1; j <= std::min(doDepth, validTests[i]); j++)
